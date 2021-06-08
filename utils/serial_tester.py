32fa9f367368e86120be18e0a7bb62d65035895d
mbt-michele-r <michele.romani@mybraintech.com>
2021-06-08 11:31:55 +0200

    :sparkles: implemented script to test available ports on Windows

---
+++ utils/serial_tester.py
@@ -0,0 +1,12 @@
+import serial
+ser = serial.Serial()
+
+for ns in range(6):
+    try:
+        ser.port = "COM" + str(ns)
+        ser.open()
+        print("COM" + str(ns+1) + " available")
+        ser.close()
+
+    except serial.SerialException:
+        print("COM" + str(ns+1) + " NOT available")
